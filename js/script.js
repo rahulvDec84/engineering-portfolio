697e7644eba41c5b55e759c458fee7034d2829c2
rahulvDec84 <143254864+rahulvDec84@users.noreply.github.com>
2026-07-18 18:21:14 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-// Rahul Kumar Verma Portfolio console.log("Portfolio Loaded Successfully"); // Smooth fade effect (future enhancement) window.addEventListener("scroll",function(){ }); function toggleProject(id, button) { const details = document.getElementById(id); if (details.style.display === "block") { details.style.display = "none"; button.innerHTML = "View Details ▼"; } else { details.style.display = "block"; button.innerHTML = "Hide Details ▲"; } }
+// Rahul Kumar Verma Portfolio 
+console.log("Portfolio Loaded Successfully"); 
+// Smooth fade effect (future enhancement) 
+window.addEventListener("scroll",function(){ 
+
+});
+ function toggleProject(id, button) 
+ { const details = document.getElementById(id); 
+   if (details.style.display === "block") { 
+   details.style.display = "none"; 
+   button.innerHTML = "View Details ▼"; 
+   } else { 
+   details.style.display = "block"; 
+   button.innerHTML = "Hide Details ▲"; 
+   } 
+}
