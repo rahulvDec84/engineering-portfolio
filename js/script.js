7a1d125b431cf7e0d66c1d943a25927991063eee
rahulvDec84 <143254864+rahulvDec84@users.noreply.github.com>
2026-07-18 18:02:23 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,14 @@ console.log("Portfolio Loaded Successfully");
 window.addEventListener("scroll",function(){
 
 });
+function toggleProject(id, button) {
+    const details = document.getElementById(id);
+
+    if (details.style.display === "block") {
+        details.style.display = "none";
+        button.innerHTML = "View Details ▼";
+    } else {
+        details.style.display = "block";
+        button.innerHTML = "Hide Details ▲";
+    }
+}
